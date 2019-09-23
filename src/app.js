f0c3acaaf11cd59209601190c0e0827b9cbb0524
Rahul joshi <joshi.rj21@gmail.com>
2019-09-23 19:12:35 +0530

    hamburger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 let togglerFlag = false;
-
+let running = false;
 var navbarToggler = document.querySelector(".expander,.expander *");
 
 navbarToggler.addEventListener("click", animeToggler)
@@ -15,6 +15,7 @@ function animeToggler() {
             height: ['0%', '100%'],
             begin: function () {
                 running = true;
+                hamRotate();
             },
             complete: function () {
                 togglerFlag = true;
@@ -34,8 +35,42 @@ function animeToggler() {
             complete: function () {
                 togglerFlag = false;
                 running = false;
+                hamReverseRotate();
             }
         });
     }
+}
+
+const hamRotate = () => {
+    let top = document.querySelector(".top");
+    let center = document.querySelector(".center")
+    let bottom = document.querySelector(".bottom");
+    center.style.display = "none";
+    anime({
+        targets: top,
+        translateY: '2.8px',
+        rotate: '45'
+    });
+    anime({
+        targets: bottom,
+        translateY: '-4.5px',
+        rotate: '-45'
+    });
+}
+const hamReverseRotate = () => {
+    let top = document.querySelector(".top");
+    let center = document.querySelector(".center")
+    let bottom = document.querySelector(".bottom");
+    center.style.display = "block";
+    anime({
+        targets: top,
+        translateY: '0',
+        rotate: '0'
+    });
+    anime({
+        targets: bottom,
+        translateY: '0',
+        rotate: '0'
+    });
 }
 

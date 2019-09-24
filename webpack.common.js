1c4b3f0e8de4b870c0156c5714cbb59ab3c66f9e
Rahul joshi <joshi.rj21@gmail.com>
2019-09-24 22:18:38 +0530

    webpack-dev-server

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,10 @@
 const path = require("path")
+const htmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    },
+    plugins: [new htmlWebpackPlugin({
+        template: "./src/template.html"
+    })],
     module: {
         rules: [
             {
